258ad6aac7b02a9bec87cca55a00d7849db46909
Yakshit Agarwal <yakshitagarwal05@gmail.com>
2026-07-10 07:53:11 +0530

    CLI commands added

---
+++ repl.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print(" >")
+
+		scanner.Scan()
+		scanner.Err()
+		text := scanner.Text()
+
+		cleaned := cleanInput(text)
+		if len(cleaned) == 0 {
+			continue
+		}
+
+		commandName := cleaned[0]
+
+		availableCommands := getCommands()
+		command, ok := availableCommands[commandName]
+		if !ok {
+			fmt.Println("Invalid command")
+			continue
+		}
+
+		command.callback()
+	}
+
+}
+
+func cleanInput(str string) []string {
+	lowered := strings.ToLower(str)
+	words := strings.Fields(lowered)
+	return words
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Prints the help menu",
+			callback:    callbackHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Prints the help menu",
+			callback:    callbackExit,
+		},
+	}
+}
